11a340a012e88a3864d85384c5950f865372096b
Rune <runebarrett@gmail.com>
2016-10-31 15:40:24 +0100

    added vsm objects

---
+++ Assets/_Scripts/StartProcessingExe.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+
+public class StartProcessingExe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Process foo = new Process();
+        foo.StartInfo.FileName = "SVSProcessingUnity2.exe";
+        foo.Start();
+    }
+}
