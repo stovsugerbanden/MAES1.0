afb216aa82671c24e1a85f6b37e8948fb2d5824d
Rune <runebarrett@gmail.com>
2016-11-30 05:08:29 +0100

    added rising water to SVS screen1 in the beginning. edits

---
+++ Assets/_Scripts/MoveWater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWater : MonoBehaviour {
+    float counter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (counter < 20)
+        {
+            counter += Time.deltaTime;
+            if (counter > 3)
+            {
+                transform.position += new Vector3(0, 0.01f, 0);
+            }
+        }
+	}
+}
